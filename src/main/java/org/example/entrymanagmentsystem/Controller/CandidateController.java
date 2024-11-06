01a506ffb4c5c2d45227a9dc024a17ebfc6eb7ef
yassin mohamed abdelmoniem <yassinmohamed022@gmail.com>
2024-11-06 18:50:52 +0200

    candidatV2

--- src/main/java/org/example/entrymanagmentsystem/Controller/CandidateController.java
+++ src/main/java/org/example/entrymanagmentsystem/Controller/CandidateController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/candidate")
+@RequestMapping("candidate")
 public class CandidateController {
     @Autowired
     CandidateService candidateService;
