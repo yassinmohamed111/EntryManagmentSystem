a3aec7490c552c4bfc119b3a28c7a94fb361d23e
yassin mohamed abdelmoniem <yassinmohamed022@gmail.com>
2024-11-07 13:24:02 +0200

    entrylog dto

---
+++ src/main/java/org/example/entrymanagmentsystem/DTO/EntryLogDTO.java
@@ -0,0 +1,14 @@
+package org.example.entrymanagmentsystem.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EntryLogDTO {
+    String nid ;
+}
