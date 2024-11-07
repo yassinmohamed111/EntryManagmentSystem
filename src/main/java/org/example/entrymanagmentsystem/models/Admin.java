958348dca37e9e81d67cd04774fe4de1442757ca
mariambakhit <mariambakhit22@gmail.com>
2024-11-07 12:21:30 +0200

    admin update

--- src/main/java/org/example/entrymanagmentsystem/models/Admin.java
+++ src/main/java/org/example/entrymanagmentsystem/models/Admin.java
@@ -17,12 +17,13 @@ public class Admin {
     @Column(name = "admin_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(unique = true)
     @Pattern(regexp = "^[^\\s@]+@[^\\s@]+\\.[^\\s@]+$", message = "Invalid email address")
     private String email;
-    @Pattern(
-            regexp = "^(?=.[A-Z])(?=.[a-z])(?=.\\d)(?=.[@$!%?&])[A-Za-z\\d@$!%?&]{8,}$",
-            message = "Password must be at least 8 characters long, include an uppercase letter, a lowercase letter, a number, and a special character."
-    )
+//    @Pattern(
+//            regexp = "^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$",
+//            message = "Password must be at least 8 characters long, include an uppercase letter, a lowercase letter, a number, and a special character."
+//    )
     private String password;
     private String user_name;
 }
