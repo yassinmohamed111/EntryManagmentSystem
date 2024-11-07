bfca5edf9ea01d515979d3d664e451bc764f202b
yassin mohamed abdelmoniem <yassinmohamed022@gmail.com>
2024-11-07 13:30:52 +0200

    entrylog dto

--- src/main/java/org/example/entrymanagmentsystem/DTO/EntryLogDTO.java
+++ src/main/java/org/example/entrymanagmentsystem/DTO/EntryLogDTO.java
@@ -1,5 +1,7 @@
 package org.example.entrymanagmentsystem.DTO;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,5 +12,8 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class EntryLogDTO {
+    @NotBlank(message = "NID cannot be blank")
+    @Pattern(regexp = "^.{14}$", message = "The field must contain exactly 14 characters.")
     String nid ;
 }
+
